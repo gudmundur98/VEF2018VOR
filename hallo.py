d7c43c8d3c1498a8a3ace667b917837bafa14c23
gudmundur98 <gudmundurbre@gmail.com>
2018-01-17 10:07:00 +0000

    Update hallo.py

--- hallo.py
+++ hallo.py
@@ -2,7 +2,21 @@ from bottle import route, run
 
 @route("/")
 def index():
-    return "<h2>Hallo Heimur</h2>"
+    return "<a href = 'about'> About</a>" \
+           "<a href = 'bio'> Biography</a>" \
+           "<a href = 'pictures'> Pictures</a>"
+
+@route("/about")
+def about():
+    return "jasdjad"
+
+@route("/bio")
+def bio():
+    return "kashdjhajds"
+
+@route("/pictures")
+def pictures():
+    return "ksjhadjahsjd"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
