3e416fa64c897d71361d903ddcb2b90b9397a270
gudmundur98 <gudmundurbre@gmail.com>
2018-01-17 09:45:37 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -18,5 +18,8 @@ def bio():
 def pictures():
     return "ksjhadjahsjd"
 
-run()
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
 
